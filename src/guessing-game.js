d77c1d63c301d4a04f1e5657db4a1831e5454ecb
potasheva-n <mad.kyon@gmail.com>
2022-08-16 19:06:14 +0500

    feat: implement class guessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.mid = Math.round((this.max+this.min)/2);
+        return this.mid;
     }
 
     lower() {
-
+        this.setRange(this.min, this.mid);
     }
 
     greater() {
-
+        this.setRange(this.mid, this.max);
     }
 }
 
